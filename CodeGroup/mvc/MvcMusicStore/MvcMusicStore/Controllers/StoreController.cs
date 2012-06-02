6ac5b05148148c0f28aa941696a27d6992fcf900
Atalia-Home <amomsen@gmail.com>
2012-06-02 19:31:44 +0000

    Added MvnMusicStore application.

---
+++ CodeGroup/mvc/MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+using System.Collections.Generic;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        public ActionResult Index()
+        {
+            var genres = new List<Genre> { 
+                new Genre { Name = "Disco" }, 
+                new Genre { Name = "Jazz" }, 
+                new Genre { Name = "Rock" } };
+            return View(genres);
+        }
+
+        //
+        // GET: /Store/Browse?genre=Disco
+        public ActionResult Browse(string genreName)
+        {
+            var genreModel = new Genre { Name = genreName };
+            return View(genreModel);
+        }
+
+        //
+        // GET: /Store/Details/5
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
+        }
+    }
+}
---
+++ CodeGroup/mvc/MvcMusicStore/MvcMusicStore/Models/Genre.cs
@@ -0,0 +1,8 @@
+﻿
+namespace MvcMusicStore.Models
+{
+    public class Genre
+    {
+        public string Name { get; set; }
+    }
+}
